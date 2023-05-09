ce12658ddc18bcf250c11453c2c5baf2227c76b6
Ricardo Quirarte <reynaldo.quirarte@gmail.com>
2023-05-09 17:12:02 -0600

    html and css ready

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import './style.css';
+
+// import { format, compareAsc } from 'date-fns'
+// format(new Date ('2023-08-30'))
+
+
+
+// Creating new todos
+function todoFactory(title, description, dueDate, priority, checklist) {
+    return { title, description, dueDate, priority, checklist };
+}
+
+const todos = [];
+
+
+const date = new Date('2023-08-30');
+let check = false;
+
+const firstTodo = todoFactory('Take a shower', 'Clean very well my colita', date, midPriority, check);
+
+firstTodo.push(projects);
+
+console.log(todos);
+
+
+// Setting todos as complete
+function todoComplete() {
+    check = !check;
+}
+
+
+// Changing todo priority
+
+const topPriority = red;
+const midPriority = orange;
+const lightProprity = green;
+
+// function changePriority() {
+//     if() {
+
+//     }
+// }
+
+
+
+// Create new Project
+// function createProject(project) {
+//     let (project) = [];
+//     (project).push(todos);
+
+//     return project;   
+// }
