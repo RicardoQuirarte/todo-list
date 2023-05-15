22a0062a9bc85a237f785980b3b0cba1f9181f6a
Ricardo Quirarte <reynaldo.quirarte@gmail.com>
2023-05-15 09:40:33 -0600

    pop up form ready

--- src/index.js
+++ src/index.js
@@ -1,52 +1,67 @@
 import './style.css';
 
-// import { format, compareAsc } from 'date-fns'
-// format(new Date ('2023-08-30'))
+// // import { format, compareAsc } from 'date-fns'
+// // format(new Date ('2023-08-30'))
 
 
 
-// Creating new todos
-function todoFactory(title, description, dueDate, priority, checklist) {
-    return { title, description, dueDate, priority, checklist };
-}
+// // Creating new todos
+// function todoFactory(title, description, dueDate, priority, checklist) {
+//     return { title, description, dueDate, priority, checklist };
+// }
 
-const todos = [];
-
-
-const date = new Date('2023-08-30');
-let check = false;
-
-const firstTodo = todoFactory('Take a shower', 'Clean very well my colita', date, midPriority, check);
-
-firstTodo.push(projects);
-
-console.log(todos);
+// const todos = [];
 
 
-// Setting todos as complete
-function todoComplete() {
-    check = !check;
-}
+// const date = new Date('2023-08-30');
+// let check = false;
+
+// const firstTodo = todoFactory('Take a shower', 'Clean very well my colita', date, midPriority, check);
+
+// firstTodo.push(projects);
+
+// console.log(todos);
 
 
-// Changing todo priority
-
-const topPriority = red;
-const midPriority = orange;
-const lightProprity = green;
-
-// function changePriority() {
-//     if() {
-
-//     }
+// // Setting todos as complete
+// function todoComplete() {
+//     check = !check;
 // }
 
 
+// // Changing todo priority
 
-// Create new Project
-// function createProject(project) {
-//     let (project) = [];
-//     (project).push(todos);
+// const topPriority = red;
+// const midPriority = orange;
+// const lightProprity = green;
 
-//     return project;   
-// }
+// // function changePriority() {
+// //     if() {
+
+// //     }
+// // }
+
+
+
+// // Create new Project
+// // function createProject(project) {
+// //     let (project) = [];
+// //     (project).push(todos);
+
+// //     return project;   
+// // }
+
+const newTodo = document.querySelector('.new-todo');
+const cancelButton = document.querySelector('.cancel-button');
+const popUpForm = document.querySelector('.pop-up-form');
+
+function popUp() {
+    popUpForm.style.display = 'flex';
+}
+
+function closePopUP() {
+    popUpForm.style.display = 'none';
+}
+
+cancelButton.addEventListener('click', closePopUP);
+newTodo.addEventListener('click', popUp);
