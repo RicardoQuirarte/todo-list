f4bfa419384934c0f28d0d231cab7fc371384327
Ricardo Quirarte <reynaldo.quirarte@gmail.com>
2023-05-24 15:25:56 -0600

    css of todos buttons ready

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { check } from "prettier";
 import "./style.css";
+import { format } from 'date-fns'
 
-// Creating new todos
-function  todoFactory(title, description, dueDate, priority) {
-  return { title, description, dueDate, priority };
-}
+// // Creating new todos
+// function  todoFactory(title, description, dueDate, priority) {
+//   return { title, description, dueDate, priority };
+// }
 
 const todos = [];
 
@@ -12,9 +13,10 @@ const todos = [];
 function getTodoFromInput() {
   const title = document.querySelector('#title').value;
   const description = document.querySelector('#description').value;
-  const dueDate = document.querySelector('#due-Date').value;
+  const newDate = new Date(`${document.querySelector('#due-Date').value}T00:00`);
+  const dueDate = format(newDate, 'dd/MMM/yy');
   const priority = document.querySelector('input[name="priority"]:checked').value;
-  return todoFactory(title, description, dueDate, priority);
+  return {title, description, dueDate, priority};
 }
 
 // Display todos
@@ -22,16 +24,17 @@ const todosDiv = document.querySelector('.todos');
 
 function displayTodos() {
   todos.forEach( userTodo => {
-    const fatherDiv = document.createElement('div');
-    fatherDiv.classList.add('each-todo');
+    const grandpaDiv = document.createElement('div');
+    grandpaDiv.classList.add('grandpa-todo');
 
+    const fatherDiv = document.createElement('div');
+    fatherDiv.classList.add('todos-side');
     const checklist = document.createElement('input');
     checklist.setAttribute('type', 'checkbox');
     checklist.classList.add('check');
     checklist.addEventListener('click', () => {
       fatherDiv.classList.toggle('line');
     });
-
     fatherDiv.appendChild(checklist);
     const div0 = document.createElement('div');
     div0.textContent = userTodo.title;
@@ -43,14 +46,31 @@ function displayTodos() {
     div2.textContent = userTodo.dueDate;
     fatherDiv.appendChild(div2);
     if(userTodo.priority === 'top') {
-      fatherDiv.classList.add('red');
+      grandpaDiv.classList.add('red');
     } else if (userTodo.priority === 'mid'){
-      fatherDiv.classList.add('orange');
+      grandpaDiv.classList.add('orange');
     } else {
-      fatherDiv.classList.add('green');
+      grandpaDiv.classList.add('green');
     }
 
-    todosDiv.appendChild(fatherDiv);
+    const motherDiv = document.createElement('div');
+    motherDiv.classList.add('buttons-side');
+    const edite = document.createElement('button');
+    edite.textContent = 'Edite'
+    edite.classList.add('todo-buttons', 'edite');
+    motherDiv.appendChild(edite);
+    const details = document.createElement('button');
+    details.textContent = 'Details'
+    details.classList.add('todo-buttons', 'details');
+    motherDiv.appendChild(details);
+    const erase = document.createElement('button');
+    erase.textContent = 'Delete'
+    erase.classList.add('todo-buttons', 'delete');
+    motherDiv.appendChild(erase);
+
+    grandpaDiv.appendChild(fatherDiv);
+    grandpaDiv.appendChild(motherDiv);
+    todosDiv.appendChild(grandpaDiv);
   })
 }
 
@@ -74,23 +94,13 @@ function addTodo() {
   todos.push(userTodo);
   popUpForm.style.display = "none";
   displayTodos();
+  console.log(todos);
 }
 
 newTodo.addEventListener("click", popUp);
 cancelButton.addEventListener("click", closePopUP);
 addButton.addEventListener('click', addTodo);
 
-
-
-
-
-
-
-
-// Playing with date-fns
-
-// import { format, compareAsc } from 'date-fns'
-// format(new Date ('2023-08-30'))
 
 
 
