47459a677031f705ff0ee5010740df559dbe29e2
Ricardo Quirarte <reynaldo.quirarte@gmail.com>
2023-05-24 12:25:53 -0600

    Set todos as ready done

--- src/index.js
+++ src/index.js
@@ -1,32 +1,98 @@
-import './style.css';
+import { check } from "prettier";
+import "./style.css";
 
-// // import { format, compareAsc } from 'date-fns'
-// // format(new Date ('2023-08-30'))
+// Creating new todos
+function  todoFactory(title, description, dueDate, priority) {
+  return { title, description, dueDate, priority };
+}
+
+const todos = [];
+
+// Get todo from input
+function getTodoFromInput() {
+  const title = document.querySelector('#title').value;
+  const description = document.querySelector('#description').value;
+  const dueDate = document.querySelector('#due-Date').value;
+  const priority = document.querySelector('input[name="priority"]:checked').value;
+  return todoFactory(title, description, dueDate, priority);
+}
+
+// Display todos
+const todosDiv = document.querySelector('.todos');
+
+function displayTodos() {
+  todos.forEach( userTodo => {
+    const fatherDiv = document.createElement('div');
+    fatherDiv.classList.add('each-todo');
+
+    const checklist = document.createElement('input');
+    checklist.setAttribute('type', 'checkbox');
+    checklist.classList.add('check');
+    checklist.addEventListener('click', () => {
+      fatherDiv.classList.toggle('line');
+    });
+
+    fatherDiv.appendChild(checklist);
+    const div0 = document.createElement('div');
+    div0.textContent = userTodo.title;
+    fatherDiv.appendChild(div0);
+    const div1 = document.createElement('div');
+    div1.textContent = userTodo.description;
+    fatherDiv.appendChild(div1);
+    const div2 = document.createElement('div');
+    div2.textContent = userTodo.dueDate;
+    fatherDiv.appendChild(div2);
+    if(userTodo.priority === 'top') {
+      fatherDiv.classList.add('red');
+    } else if (userTodo.priority === 'mid'){
+      fatherDiv.classList.add('orange');
+    } else {
+      fatherDiv.classList.add('green');
+    }
+
+    todosDiv.appendChild(fatherDiv);
+  })
+}
+
+// DOM for new todo pop up form
+const newTodo = document.querySelector(".new-todo");
+const popUpForm = document.querySelector(".pop-up-form");
+const cancelButton = document.querySelector(".cancel-button");
+const addButton = document.querySelector('.add-todo');
+
+
+function popUp() {
+  popUpForm.style.display = "flex";
+}
+
+function closePopUP() {
+  popUpForm.style.display = "none";
+}
+
+function addTodo() {
+  const userTodo = getTodoFromInput();
+  todos.push(userTodo);
+  popUpForm.style.display = "none";
+  displayTodos();
+}
+
+newTodo.addEventListener("click", popUp);
+cancelButton.addEventListener("click", closePopUP);
+addButton.addEventListener('click', addTodo);
 
 
 
-// // Creating new todos
-// function todoFactory(title, description, dueDate, priority, checklist) {
-//     return { title, description, dueDate, priority, checklist };
-// }
-
-// const todos = [];
 
 
-// const date = new Date('2023-08-30');
-// let check = false;
-
-// const firstTodo = todoFactory('Take a shower', 'Clean very well my colita', date, midPriority, check);
-
-// firstTodo.push(projects);
-
-// console.log(todos);
 
 
-// // Setting todos as complete
-// function todoComplete() {
-//     check = !check;
-// }
+
+// Playing with date-fns
+
+// import { format, compareAsc } from 'date-fns'
+// format(new Date ('2023-08-30'))
+
+
 
 
 // // Changing todo priority
@@ -35,33 +101,19 @@ import './style.css';
 // const midPriority = orange;
 // const lightProprity = green;
 
-// // function changePriority() {
-// //     if() {
+// function changePriority() {
+//     if() {
 
-// //     }
-// // }
+//     }
+// }
+
 
 
 
 // // Create new Project
-// // function createProject(project) {
-// //     let (project) = [];
-// //     (project).push(todos);
+// function createProject(project) {
+//     let (project) = [];
+//     (project).push(todos);
 
-// //     return project;   
-// // }
-
-const newTodo = document.querySelector('.new-todo');
-const cancelButton = document.querySelector('.cancel-button');
-const popUpForm = document.querySelector('.pop-up-form');
-
-function popUp() {
-    popUpForm.style.display = 'flex';
-}
-
-function closePopUP() {
-    popUpForm.style.display = 'none';
-}
-
-cancelButton.addEventListener('click', closePopUP);
-newTodo.addEventListener('click', popUp);
+//     return project;
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
@@ -16,7 +16,7 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
